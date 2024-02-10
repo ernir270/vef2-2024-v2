cacdaede7f0630f686991ebd1441a8c3c9158f50
ernir270 <ernir.styrmisson@gmail.com>
2024-02-10 21:44:20 +0000

    sql insert komið

---
+++ sql/insert.sql
@@ -0,0 +1,100 @@
+INSERT INTO teams (name) VALUES ('Boltaliðið');
+INSERT INTO teams (name) VALUES ('Dripplararnir');
+INSERT INTO teams (name) VALUES ('Skotföstu kempurnar');
+INSERT INTO teams (name) VALUES ('Markaskorarnir');
+INSERT INTO teams (name) VALUES ('Sigurliðið');
+INSERT INTO teams (name) VALUES ('Risaeðlurnar');
+INSERT INTO teams (name) VALUES ('Framherjarnir');
+INSERT INTO teams (name) VALUES ('Fljótu fæturnir');
+INSERT INTO teams (name) VALUES ('Vinningshópurinn');
+INSERT INTO teams (name) VALUES ('Ósigrandi skotfólkið');
+INSERT INTO teams (name) VALUES ('Óhemjurnar');
+INSERT INTO teams (name) VALUES ('Hraðaliðið');
+
+--3 | Boltaliðið
+--4 | Dripplararnir
+--5 | Skotföstu kempurnar
+--6 | Markaskorarnir
+--7 | Sigurliðið
+--8 | Risaeðlurnar
+--9 | Framherjarnir
+--10 | Fljótu fæturnir
+--11 | Vinningshópurinn
+--12 | Ósigrandi skotfólkið
+--13 | Óhemjurnar
+--14 | Hraðaliðið
+
+--skrá: xj39
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-13T15:20:53.955Z', 5, 12, 0, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-13T15:20:53.955Z', 11, 3, 2, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-13T15:20:53.955Z', 8, 6, 2, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-13T15:20:53.955Z', 7, 9, 5, 2);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-13T15:20:53.955Z', 14, 10, 2, 3);
+
+--skrá: z48d
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 13, 6, 4, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 3, 12, 4, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 4, 8, 4, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 5, 7, 1, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 10, 9, 1, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-23T15:20:53.955Z', 11, 14, 0, 4);
+
+--skrá: nxl3
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 6, 9, 4, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 8, 11, 2, 4);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 13, 14, 5, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 5, 3, 0, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 7, 10, 1, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-27T15:20:53.955Z', 4, 12, 2, 4);
+
+--skrá:llx8
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-30T15:20:53.955Z', 12, 7, 2, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-30T15:20:53.955Z', 13, 4, 4, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-30T15:20:53.955Z', 8, 11, 1, 1);
+
+--skrá: 9099
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 13, 7, 0, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 5, 11, 0, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 9, 8, 0, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 12, 3, 0, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 10, 14, 1, 2);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-07T15:20:53.955Z', 6, 4, 2, 1);
+
+--skrá: 4828
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 12, 9, 0, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 10, 4, -1, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 3, 6, 0, {});
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 7, 5, 3, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 8, 13, 4, 4);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-15T15:20:53.955Z', 11, 14, 4, 1);
+
+--skrá: 1230
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 12, 14, 0, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 13, 10, 0, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 7, 11, 0,3 );
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 9, 4, 5, 2);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 3, 5, 3, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-02T15:20:53.955Z', 6, 8, 5, 4);
+
+--skrá: 9xnf
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-23T15:20:53.955Z', 4, 6, 0, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-23T15:20:53.955Z', 10, 12, 1, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-23T15:20:53.955Z', 14, 3, 4, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-23T15:20:53.955Z', 5, 7, 1, 4);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-23T15:20:53.955Z', 8, 13, 1, 2);
+
+--skrá: 8k38
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 7, 9, 2, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 11, 8, 5, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 3, 4, 5, 2);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 10, 14, 3, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 5, 12, 1, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-02-16T15:20:53.955Z', 13, 6, 2, 0);
+
+--skrá: 3iks
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 5, 14, 5, 4);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 10, 9, 1, 0);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 6, 12, 2, 5);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 3, 8, 5, 1);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 4, 7, 5, 3);
+INSERT INTO games (date, home, away, home_score, away_score) values ('2024-01-22T15:20:53.955Z', 11, 13, 3, 2);
